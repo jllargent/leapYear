2defd36ee70cf3b1911f43d0edde4eb748d4b0d2
Jon Largent <jllargent@bsu.edu>
2016-01-15 08:18:52 -0500

    Second commit

--- test/edu/bsu/cs222/LeapYearCalculatorTest.java
+++ test/edu/bsu/cs222/LeapYearCalculatorTest.java
@@ -1,11 +1,11 @@
 package edu.bsu.cs222;
 
-import junit.framework.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LeapYearCalculatorTest {
     @Test
-    public void test2015IsALeapYear(){
+    public void test2016IsALeapYear(){
         LeapYearCalculator calc = new LeapYearCalculator();
         Assert.assertTrue(calc.isLeapYear(2016));
     }
