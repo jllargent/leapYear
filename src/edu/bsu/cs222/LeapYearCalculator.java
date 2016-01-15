183d7c603ee0cb7bb391a23f625e353bfd2156c7
Jon Largent <jllargent@bsu.edu>
2016-01-15 08:46:54 -0500

    third commit

--- src/edu/bsu/cs222/LeapYearCalculator.java
+++ src/edu/bsu/cs222/LeapYearCalculator.java
@@ -4,7 +4,16 @@ package edu.bsu.cs222;
  * Created by jon on 1/13/16.
  */
 public class LeapYearCalculator {
-    public boolean isLeapYear(int year){
-        return true;
+    public boolean isLeapYear(int year) {
+        if (isDivisibleByFour(year)) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private boolean isDivisibleByFour(int year) {
+        return year % 4 == 0;
     }
 }
+
--- test/edu/bsu/cs222/LeapYearCalculatorTest.java
+++ test/edu/bsu/cs222/LeapYearCalculatorTest.java
@@ -4,9 +4,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LeapYearCalculatorTest {
+
+    private LeapYearCalculator calc = new LeapYearCalculator();
+
     @Test
-    public void test2016IsALeapYear(){
-        LeapYearCalculator calc = new LeapYearCalculator();
+    public void test2016IsALeapYear() {
         Assert.assertTrue(calc.isLeapYear(2016));
     }
+
+    @Test
+    public void test2015IsNotALeapYear() {
+        Assert.assertFalse(calc.isLeapYear(2015));
+    }
+
+    @Test
+    public void test2000IsALeapYear() {
+        Assert.assertTrue(calc.isLeapYear(2000));
+    }
+
+    @Test
+    public void test1900IsNotALeapYear() {
+        Assert.assertFalse(calc.isLeapYear(1900));
+    }
+
+    @Test
+    public void test1752IsALeapYear() {
+        Assert.assertTrue(calc.isLeapYear(1752));
+    }
+
+
 }
